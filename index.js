eea7a3ae8289cbd96fd0cdc4ed0d73af869576f9
aditya jha <85640832+jhaadit@users.noreply.github.com>
2022-07-02 21:03:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+document.querySelector("button").addEventListener("click", playGame);
+
+function playGame() {
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+  var randomDiceImage1 = "dice" + randomNumber1 + ".png";
+
+  var randomDiceSrc1 = "images/" + randomDiceImage1;
+
+  var image1 = document.querySelectorAll("img")[0];
+
+  image1.setAttribute("src", randomDiceSrc1);
+
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+  var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+
+  var randomDiceSrc2 = "images/" + randomDiceImage2;
+
+  var image2 = document.querySelectorAll("img")[1];
+
+  image2.setAttribute("src", randomDiceSrc2);
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 wins!";
+  }
+  else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 win! 🚩";
+  }
+  else {
+    document.querySelector("h1").innerHTML = "🚩 Draw! 🚩";
+  }
+
+}
